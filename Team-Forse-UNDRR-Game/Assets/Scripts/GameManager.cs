7f11c67e48c3bdf387d102e5781bc1a1d1790ac0
Trevor (TJ) <trevorebeckham@gmail.com>
2025-02-03 17:06:49 -0500

    - Initial Doc + game manager

---
+++ Team-Forse-UNDRR-Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float cityFunds = 100f;
+    public float cityIncome = 100f;
+    public int popAlive = 5000000;
+    public int popInjured = 0;
+    public int popDead = 0;
+    public int workforceActive = 3;
+    public int workforceIdle = 0;
+    public int playerActions = 5;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
