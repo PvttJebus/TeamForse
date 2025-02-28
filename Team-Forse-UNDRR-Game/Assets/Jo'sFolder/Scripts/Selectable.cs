f994ed063032dc182b3479c2cdc311afb600cc4a
HusackJo <161376370+HusackJo@users.noreply.github.com>
2025-02-28 18:19:09 -0500

    Camera & Selections
    2/28

--- Team-Forse-UNDRR-Game/Assets/Jo'sFolder/Scripts/Selectable.cs
+++ Team-Forse-UNDRR-Game/Assets/Jo'sFolder/Scripts/Selectable.cs
@@ -1,25 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class Selectable : MonoBehaviour
 {
     public GameObject menuToSpawn;
-
+    public Vector3 cameraOffset;
     //public MenuManager menuManager;
     public bool isSelected;
 
-    private void OnMouseDown()
+    public Vector3 GetSelectionCameraPosition()
+    {
+        return gameObject.transform.position + cameraOffset;
+    }
+    public void Select()
     {
         isSelected = true;
-        Instantiate(menuToSpawn);
-        //menuManager.selectedObject = this;
+        //menuToSpawn.SetActive(true);
     }
 
     public void Unselect()
     {
         isSelected = false;
-        //menuManager.selectedObject = null;
     }
 }
--- Team-Forse-UNDRR-Game/Assets/Scripts/CameraMovement.cs
+++ Team-Forse-UNDRR-Game/Assets/Scripts/CameraMovement.cs
@@ -1,25 +1,22 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CameraSwitcher : MonoBehaviour
 {
-    public Transform[] cameraPositions; // Array of camera positions
-    public int defaultPositionIndex = 0; // Index of the default camera position
-    private Camera mainCamera; // Reference to the main camera
+    [Header("Smoothing")]
+    [SerializeField]
+    public float smoothingSpeed;
+    [Header("Camera")]
+    public Vector3 defaultCameraPosition;
+    Vector3 cameraTargetPosition;
+    Selectable currentSelection;
+    Camera mainCamera; 
 
     void Start()
     {
-        mainCamera = Camera.main; // Get the main camera
-
-        // Set the camera to the default position if valid
-        if (cameraPositions.Length > 0 && defaultPositionIndex < cameraPositions.Length)
-        {
-            SetCameraPosition(defaultPositionIndex);
-        }
-        else
-        {
-            Debug.LogWarning("No camera positions assigned or invalid default index.");
-        }
+        mainCamera = Camera.main; 
+        cameraTargetPosition = mainCamera.transform.position;
     }
 
     void Update()
@@ -27,30 +24,58 @@ public class CameraSwitcher : MonoBehaviour
         // Check for left mouse button click
         if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition); // Cast a ray from the camera to the mouse position
-            RaycastHit hit;
+            RaycastToTrigger();
+        }
+        SmoothCameraMovement();
+    }
 
-            // Check if the ray hits an object
-            if (Physics.Raycast(ray, out hit))
+    private void SmoothCameraMovement()
+    {
+        Vector3 smoothedPosition = Vector3.Lerp(cameraTargetPosition, mainCamera.transform.position, smoothingSpeed);
+        mainCamera.gameObject.transform.position = smoothedPosition;
+    }
+
+    private void RaycastToTrigger()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition); // Cast a ray from the camera to the mouse position
+        RaycastHit hit;
+
+        // Check if the ray hits an object
+        if (Physics.Raycast(ray, out hit))
+        {
+            //Did we hit a Selectable Object?
+            Selectable selectable = hit.collider.gameObject.GetComponent<Selectable>();
+            if (selectable != null)
             {
-                CameraPositionTrigger trigger = hit.collider.GetComponent<CameraPositionTrigger>(); // Get the CameraPositionTrigger component
-
-                // If the object has a CameraPositionTrigger, switch to the assigned position
-                if (trigger != null)
-                {
-                    SetCameraPosition(trigger.positionIndex);
-                }
+                currentSelection = selectable;
+                currentSelection.Select();
+                SetTargetToSelectable(currentSelection);
+            }
+            else
+            {
+                SetTargetToDefault();
             }
         }
-    }
-
-    // Method to set the camera position and rotation based on the index
-    public void SetCameraPosition(int index)
-    {
-        if (index >= 0 && index < cameraPositions.Length)
+        //if we didn't hit any objects, we deselect & set target to default
+        else
         {
-            mainCamera.transform.position = cameraPositions[index].position;
-            mainCamera.transform.rotation = cameraPositions[index].rotation;
+            SetTargetToDefault();
         }
     }
+
+    private void SetTargetToSelectable(Selectable selectable)
+    {
+        cameraTargetPosition = selectable.GetSelectionCameraPosition();
+        print($"Camera Target Position: {cameraTargetPosition}");
+
+        //do rotation too don't just leave this comment
+    }
+
+    private void SetTargetToDefault()
+    {
+        cameraTargetPosition = defaultCameraPosition;
+        currentSelection.Unselect();
+        currentSelection = null;
+    }
+
 }
